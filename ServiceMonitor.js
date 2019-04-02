7f08c3ee111c66b638df4cfb9c85e9c26e4a4dfa
Sam Ali-mirsalari <salimirs@uwo.ca>
2019-04-02 18:16:34 -0400

    gets list of services

--- ServiceMonitor.js
+++ ServiceMonitor.js
@@ -4,6 +4,21 @@ var serviceAccount = require('./keys/serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-  });
-  
+});
+
 var db = admin.firestore();
+
+
+var services = db.collection('services');
+var getAvailableService = services.doc('available_services')
+getAvailableService.get()
+.then(doc => {
+    if (!doc.exists) {
+        console.log('No such document!');
+    } else {
+        console.log('Document data:', doc.data());
+    }
+})
+.catch(err => {
+    console.log('Error getting document', err);
+});
